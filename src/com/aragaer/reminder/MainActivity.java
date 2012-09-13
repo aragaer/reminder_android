c1b3915e8d8f3cac3783a1daf640d1c114855152
Ilya Konovalov <i.konovalov@samsung.com>
2012-09-13 11:42:32 +0400

    Initial commit

---
+++ src/com/aragaer/reminder/MainActivity.java
@@ -0,0 +1,11 @@
+package com.aragaer.reminder;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
