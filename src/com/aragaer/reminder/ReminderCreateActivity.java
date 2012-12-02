a493391b4552ed8699d68812c87d73f4bf1bded6
Ilya Konovalov <aragaer@gmail.com>
2012-12-02 18:09:28 +0400

    Another iteration on 'add extra information'

--- src/com/aragaer/reminder/ReminderCreateActivity.java
+++ src/com/aragaer/reminder/ReminderCreateActivity.java
@@ -5,32 +5,23 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.ActionProvider;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.MenuItem.OnMenuItemClickListener;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.Toast;
 
 public class ReminderCreateActivity extends Activity {
 	DrawView dv;
 	ColorSwitch cs;
-	boolean extra;
 
 	@SuppressLint("NewApi")
 	public void onCreate(Bundle savedInstanceState) {
@@ -86,42 +77,39 @@ public class ReminderCreateActivity extends Activity {
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuItem extra_item = menu.add(R.string.add_extra);
-		extra_item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			public boolean onMenuItemClick(MenuItem item) {
-				extra = !extra;
-				if (extra) {
-					item.setIcon(android.R.drawable.ic_menu_edit);
-					Toast.makeText(ReminderCreateActivity.this, R.string.add_extra, Toast.LENGTH_SHORT).show();
-				} else {
-					item.setIcon(android.R.drawable.ic_menu_save);
-					Toast.makeText(ReminderCreateActivity.this, R.string.no_extra, Toast.LENGTH_SHORT).show();
-				}
-				return true;
-			}
-		});
-		extra_item.setIcon(android.R.drawable.ic_menu_save);
-		extra_item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-		MenuItem done = menu.add(R.string.add_extra);
+		MenuItem done = menu.add(R.string.no_extra);
 		done.setOnMenuItemClickListener(new OnMenuItemClickListener() {
 			public boolean onMenuItemClick(MenuItem item) {
-				Bitmap res = dv.getBitmap();
-				dv.reset();
-
-				ContentValues row = new ContentValues();
-				row.put("glyph", ReminderItem.bitmap_to_bytes(res));
-				row.put("date", System.currentTimeMillis());
-				row.put("color", cs.getValue());
-				Uri result_uri = getContentResolver().insert(ReminderProvider.content_uri, row);
-				if (extra)
-					startActivity(new Intent(ReminderCreateActivity.this, ReminderViewActivity.class)
-							.putExtra("reminder_id", ContentUris.parseId(result_uri)));
-				ReminderCreateActivity.this.finish();
+				save(false);
 				return true;
 			}
 		});
 		done.setIcon(R.drawable.ic_cab_done_holo_dark);
 		done.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		MenuItem next = menu.add(R.string.add_extra);
+		next.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			public boolean onMenuItemClick(MenuItem item) {
+				save(true);
+				return true;
+			}
+		});
+		next.setIcon(R.drawable.navigation_forward);
+		next.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 		return true;
+	}
+
+	void save(boolean extra) {
+		Bitmap res = dv.getBitmap();
+		dv.reset();
+
+		ContentValues row = new ContentValues();
+		row.put("glyph", ReminderItem.bitmap_to_bytes(res));
+		row.put("date", System.currentTimeMillis());
+		row.put("color", cs.getValue());
+		Uri result_uri = getContentResolver().insert(ReminderProvider.content_uri, row);
+		if (extra)
+			startActivity(new Intent(ReminderCreateActivity.this, ReminderViewActivity.class)
+				.putExtra("reminder_id", ContentUris.parseId(result_uri)));
+		ReminderCreateActivity.this.finish();
 	}
 }
