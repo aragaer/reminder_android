d370163d760399e7f5d8c9cce37cbb65179c3f4a
Ilya Konovalov <aragaer@gmail.com>
2012-12-23 22:58:50 +0400

    Change CreateActivity for smaller diff with 3.0+

--- src/com/aragaer/reminder/ReminderCreateActivity.java
+++ src/com/aragaer/reminder/ReminderCreateActivity.java
@@ -3,6 +3,7 @@ package com.aragaer.reminder;
 import com.aragaer.simpleactionbar.AbActivity;
 import com.aragaer.simpleactionbar.ActionBar;
 
+import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -21,14 +22,11 @@ public class ReminderCreateActivity extends AbActivity {
 	DrawView dv;
 	ColorSwitch cs;
 
+	@SuppressLint("NewApi")
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ActionBar ab = getActionBar();
-		ab.setDisplayHomeAsUpEnabled(true);
-		ab.setTitle(R.string.app_name);
-
-		LinearLayout inner = new LinearLayout(this);
-		inner.setGravity(Gravity.CENTER);
+		LinearLayout ll = new LinearLayout(this);
+		ll.setGravity(Gravity.CENTER);
 
 		cs = new ColorSwitch(this);
 		cs.setId(3);
@@ -38,10 +36,10 @@ public class ReminderCreateActivity extends AbActivity {
 		dv.setId(1);
 
 		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-			inner.setOrientation(LinearLayout.VERTICAL);
+			ll.setOrientation(LinearLayout.VERTICAL);
 			cs.setOrientation(LinearLayout.HORIZONTAL);
 		} else {
-			inner.setOrientation(LinearLayout.HORIZONTAL);
+			ll.setOrientation(LinearLayout.HORIZONTAL);
 			cs.setOrientation(LinearLayout.VERTICAL);
 		}
 
@@ -56,37 +54,18 @@ public class ReminderCreateActivity extends AbActivity {
 			}
 		});
 
-		inner.addView(cs);
-		inner.addView(dv);
-		setContentView(inner);
+		ll.addView(cs);
+		ll.addView(dv);
+		setContentView(ll);
+
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		ab.setTitle(R.string.app_name);
 	}
 
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putParcelable("image", dv.getBitmap());
-	}
-
-	void save(boolean extra) {
-		Bitmap res = dv.getBitmap();
-		dv.reset();
-
-		ContentValues row = new ContentValues();
-		row.put("glyph", ReminderItem.bitmap_to_bytes(res));
-		row.put("date", System.currentTimeMillis());
-		row.put("color", cs.getValue());
-		Uri result_uri = getContentResolver().insert(
-				ReminderProvider.content_uri, row);
-		if (extra)
-			startActivity(new Intent(ReminderCreateActivity.this,
-					ReminderViewActivity.class).putExtra("reminder_id",
-					ContentUris.parseId(result_uri)));
-		finish();
-	}
-
-	public boolean onCreateActionBarMenu(Menu menu) {
-		menu.add(R.string.no_extra).setIcon(R.drawable.ic_cab_done_holo_dark);
-		menu.add(R.string.add_extra).setIcon(R.drawable.navigation_forward);
-		return true;
 	}
 
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -104,5 +83,28 @@ public class ReminderCreateActivity extends AbActivity {
 			break;
 		}
 		return true;
+	}
+
+	public boolean onCreateActionBarMenu(Menu menu) {
+		menu.add(R.string.no_extra).setIcon(R.drawable.ic_cab_done_holo_dark);
+		menu.add(R.string.add_extra).setIcon(R.drawable.navigation_forward);
+		return true;
+	}
+
+	void save(boolean extra) {
+		Bitmap res = dv.getBitmap();
+		dv.reset();
+
+		ContentValues row = new ContentValues();
+		row.put("glyph", ReminderItem.bitmap_to_bytes(res));
+		row.put("date", System.currentTimeMillis());
+		row.put("color", cs.getValue());
+		Uri result_uri = getContentResolver().insert(
+				ReminderProvider.content_uri, row);
+		if (extra)
+			startActivity(new Intent(ReminderCreateActivity.this,
+					ReminderViewActivity.class).putExtra("reminder_id",
+					ContentUris.parseId(result_uri)));
+		finish();
 	}
 }
